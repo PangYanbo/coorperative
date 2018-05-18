368593b8ef5bb60e90276ff3de82c999d588e09a
PangYanbo <pybdtc@gmail.com>
2018-05-18 20:29:16 +0900

    20180518

--- cooperative/src/process/Extractfiles.java
+++ cooperative/src/process/Extractfiles.java
@@ -18,17 +18,23 @@ public class Extractfiles {
 			ex.printStackTrace();
 		}
 		
-		File out = new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_"+date);
-		out.renameTo(new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_"+date+".csv"));
-		File out2 =   new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_"+date);
-	    out2.renameTo(new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_"+date+".csv"));
+		File out = new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_" + date);
+		out.renameTo(new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_" + date+".csv"));
+		File out2 =   new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_" + date);
+	    out2.renameTo(new File("/home/t-iho/grid/0/tmp/ktsubouc/gps_" + date+".csv"));
 	}
 	
 	public static void main(String[] args){
-		System.out.println("Enter the date");
+		System.out.println("Enter the year and month (e.g.:201604)");
 		Scanner in = new Scanner(System.in);
-		String day = in.nextLine();
-		extractfile(day);
+		String year_month = in.nextLine();
+		System.out.println("Enter the number of days(7 days as 7)");
+		String days = in.nextLine();
+		for(int i = 1; i <= Integer.valueOf(days); i++){
+			String day =(i<10)? "0"+Integer.toString(i):Integer.toString(i);
+			String date = year_month+day;
+			extractfile(date);
+		}
 	}
 	
 }
